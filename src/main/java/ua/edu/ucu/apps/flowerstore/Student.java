5a9af1683a26ee2a2fa5f9fe88c075eb4f02bafa
Viktor Pakholok <pakholok.pn@ucu.edu.ua>
2024-11-11 22:16:31 +0200

    Commented files from lesson

--- src/main/java/ua/edu/ucu/apps/flowerstore/Student.java
+++ src/main/java/ua/edu/ucu/apps/flowerstore/Student.java
@@ -1,30 +1,30 @@
-package ua.edu.ucu.apps.flowerstore;
+// package ua.edu.ucu.apps.flowerstore;
 
-import java.time.LocalDate;
+// import java.time.LocalDate;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
-import lombok.Getter;
-import lombok.Setter;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.Table;
+// import jakarta.persistence.Transient;
+// import lombok.Getter;
+// import lombok.Setter;
 
-@Getter @Setter
-@Entity
-@Table
-public class Student {
-    String firstName;
-    String secondName;
-    String gender;
-    LocalDate dateOfBirth;
+// @Getter @Setter
+// @Entity
+// @Table
+// public class Student {
+//     String firstName;
+//     String secondName;
+//     String gender;
+//     LocalDate dateOfBirth;
 
-    @Transient
-    int age;
+//     @Transient
+//     int age;
     
-    public int getAge() {
-        return age;
-    }
+//     public int getAge() {
+//         return age;
+//     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-}
+//     public void setAge(int age) {
+//         this.age = age;
+//     }
+// }
--- src/main/java/ua/edu/ucu/apps/flowerstore/StudentRepository.java
+++ src/main/java/ua/edu/ucu/apps/flowerstore/StudentRepository.java
@@ -1,12 +1,12 @@
-package ua.edu.ucu.apps.flowerstore;
+// package ua.edu.ucu.apps.flowerstore;
 
-import java.util.Optional;
+// import java.util.Optional;
 
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
+// import org.springframework.data.jpa.repository.Query;
+// import org.springframework.stereotype.Repository;
 
-@Repository
-public interface StudentRepository {
-    @Query("SELECT s FROM student s WHERE s.firstName = :firstName")
-    public Optional<Student> findFirstName(String firstName);
-}
+// @Repository
+// public interface StudentRepository {
+//     @Query("SELECT s FROM student s WHERE s.firstName = :firstName")
+//     public Optional<Student> findFirstName(String firstName);
+// }
